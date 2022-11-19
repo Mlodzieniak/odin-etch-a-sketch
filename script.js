6737360f3a3ba9f2751f26fb7dffd72542db7001
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2022-11-19 19:55:40 +0100

    grid resize feature with slider

--- script.js
+++ script.js
@@ -1,37 +1,68 @@
 const gridContainer = document.querySelector('.grid-container');
 const erase = document.querySelector('#erase');
-const gridSize = document.querySelector('#grid-size');
+const gridSizeSlider = document.getElementById('grid-size-slider')
+const gridSizeValueOutput = document.getElementById('grid-size-value-output')
+const meshToggle = document.querySelector('#mesh-toggle');
 
-
-
+gridSizeValueOutput.innerHTML = gridSizeSlider.value;
+gridSizeSlider.oninput = function(){
+    gridSizeValueOutput.innerHTML=this.value;
+    setGridSize();
+    meshFeature();
+}
 setGridSize();
-function drawGrid (sideLength){
-    size=sideLength**2;
-    for(i=0; i<size; i++){
+function setGridSize() {
+    gridContainer.innerHTML = '';
+    let correctGridSize = gridSizeSlider.value;
+    drawGrid(correctGridSize);
+    const block = document.querySelectorAll('.block-element');
+    addEventListenerToEveryElement(block, correctGridSize);
+}
+function drawGrid(sideLength) {
+    size = sideLength ** 2;
+    for (i = 0; i < size; i++) {
         const blockElement = document.createElement('div');
         blockElement.classList.add('block-element');
         gridContainer.appendChild(blockElement);
     }
 }
-function addEventListenerToEveryElement(block){
-    block.forEach(element =>{
-        element.addEventListener('click', ()=>{
-            element.classList.toggle('block-hover');
+function addEventListenerToEveryElement(block, sizeSetupPrompt) {
+    block.forEach(element => {
+        element.addEventListener('mousedown', () => {
+            element.classList.add('block-hover');
+            // element.addEventListener('mouseover', ()=>{ 
+            //     element.classList.add('block-hover');
+            // })
         })
     });
+    block.forEach(element => {
+        element.style.cssText = `width: ${100 / sizeSetupPrompt}%; height: ${100 / sizeSetupPrompt}%`
+    })
 }
-function setGridSize(){
-    gridContainer.innerHTML = '';
-    let sizeSetupPrompt = prompt('How many squares per side do you want to create?(max 100)')
-    drawGrid(sizeSetupPrompt);
+function gridSizeCheck() {
+    let sizeSetupPrompt = 0;
+    while (true) {
+        sizeSetupPrompt = prompt('Type grid size. Between 1 and 100.')
+        if (sizeSetupPrompt > 100 || sizeSetupPrompt < 1) {
+            alert('Type between 1 and 100.');
+            continue;
+        } break;
+    }
+    return sizeSetupPrompt
+}
+
+function eraseGrid() {
     const block = document.querySelectorAll('.block-element');
-    addEventListenerToEveryElement(block);
-}
-function eraseGrid(){
     block.forEach(element => {
         element.classList.remove('block-hover');
     })
 };
+function meshFeature(){
+    const block = document.querySelectorAll('.block-element');
+    block.forEach(element => {
+        element.classList.toggle('block-mesh');
+    })
+}
 
 erase.addEventListener('click', eraseGrid);
-gridSize.addEventListener('click', setGridSize);
+meshToggle.addEventListener('click', meshFeature);
