8c7854616032bb29386a11c3e0a5f99da3740226
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2022-11-19 10:10:34 +0100

    add coloring individual squares and erase feature

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const gridContainer = document.querySelector('.grid-container');
+const erase = document.querySelector('#erase');
 
 const drawGrid = (sideLength) => {
     size=sideLength**2
     for(i=0; i<size; i++){
-        const block = document.createElement('div')
-        gridContainer.appendChild(block);
-        // gridContainer.innerHTML = `<div class=block${size}>A</div>`
+        const blockElement = document.createElement('div')
+        blockElement.classList.add('block-element')
+        gridContainer.appendChild(blockElement);
     }
 }
 drawGrid(16);
+const block = document.querySelectorAll('.block-element');
+block.forEach(element =>{
+    element.addEventListener('click', ()=>{
+        element.classList.toggle('block-hover');
+    })
+});
+
+const eraseGrid = () =>{
+    block.forEach(element => {
+        element.classList.remove('block-hover');
+    })
+};
+erase.addEventListener('click', eraseGrid);
