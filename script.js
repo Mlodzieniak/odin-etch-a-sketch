434170081f8cb85e233d3157aeab382db4e240ad
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2022-11-19 10:58:06 +0100

    grid resize feature, but sill need to adjust elements size

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 const gridContainer = document.querySelector('.grid-container');
 const erase = document.querySelector('#erase');
+const gridSize = document.querySelector('#grid-size');
 
-const drawGrid = (sideLength) => {
-    size=sideLength**2
+
+
+setGridSize();
+function drawGrid (sideLength){
+    size=sideLength**2;
     for(i=0; i<size; i++){
-        const blockElement = document.createElement('div')
-        blockElement.classList.add('block-element')
+        const blockElement = document.createElement('div');
+        blockElement.classList.add('block-element');
         gridContainer.appendChild(blockElement);
     }
 }
-drawGrid(16);
-const block = document.querySelectorAll('.block-element');
-block.forEach(element =>{
-    element.addEventListener('click', ()=>{
-        element.classList.toggle('block-hover');
-    })
-});
-
-const eraseGrid = () =>{
+function addEventListenerToEveryElement(block){
+    block.forEach(element =>{
+        element.addEventListener('click', ()=>{
+            element.classList.toggle('block-hover');
+        })
+    });
+}
+function setGridSize(){
+    gridContainer.innerHTML = '';
+    let sizeSetupPrompt = prompt('How many squares per side do you want to create?(max 100)')
+    drawGrid(sizeSetupPrompt);
+    const block = document.querySelectorAll('.block-element');
+    addEventListenerToEveryElement(block);
+}
+function eraseGrid(){
     block.forEach(element => {
         element.classList.remove('block-hover');
     })
 };
+
 erase.addEventListener('click', eraseGrid);
+gridSize.addEventListener('click', setGridSize);
